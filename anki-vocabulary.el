5d6abc071a1aa80b9cf04ba82e207be898630347
darksun <lujun9972@gmail.com>
2019-11-01 22:30:37 +0800

    rename to anki-vocabulary

--- anki-vocabulary.el
+++ anki-vocabulary.el
@@ -1,12 +1,13 @@
-;;; anki-helper.el --- help you to create vocabulary card in Anki
+;;; anki-vocabulary.el --- Help you to create vocabulary card in Anki
 
 ;; Copyright (C) 2019-2019 DarkSun <lujun9972@gmail.com>.
 
 ;; Author: DarkSun <lujun9972@gmail.com>
 ;; Keywords: lisp, anki, translator, chinese
-;; Package: pdf-anki-helper
+;; Package: anki-vocabulary
 ;; Version: 1.0
-;; Package-Requires: ((emacs "24.3") (s "1.0") (youdao-dictionary "20180714") (AnkiConnect "1.0"))
+;; Package-Requires: ((emacs "24.4") (s "1.0") (youdao-dictionary "0.4") (AnkiConnect "1.0"))
+;; URL: http://github.com/lujun9972/anki-vocabulary.el
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -25,13 +26,13 @@
 
 ;;; Source code
 ;;
-;; anki-helper's code can be found here:
-;;   http://github.com/lujun9972/anki-helper.el
+;; anki-vocabulary's code can be found here:
+;;   http://github.com/lujun9972/anki-vocabulary.el
 
 ;;; Commentary:
 
-;; anki-helper is a plugin that help you to create vocabulary card in Anki
-;; Just select the sentence then execute =M-x anki-helper=
+;; anki-vocabulary is a plugin that help you to create vocabulary card in Anki
+;; Just select the sentence then execute =M-x anki-vocabulary=
 
 
 ;;; Code:
@@ -40,29 +41,29 @@
 (require 'youdao-dictionary)
 (require 'AnkiConnect)
 
-(defgroup pdf-anki-helper nil
+(defgroup anki-vocabulary nil
   ""
-  :prefix "anki-helper"
+  :prefix "anki-vocabulary"
   :group 'tools
-  :link '(url-link :tag "Github" "https://github.com/lujun9972/pdf-anki-helper.el"))
+  :link '(url-link :tag "Github" "https://github.com/lujun9972/pdf-anki-vocabulary.el"))
 
-(defcustom anki-helper-deck-name ""
-  "Which deck would the word stored"
+(defcustom anki-vocabulary-deck-name ""
+  "Which deck would the word stored."
   :type 'string)
 
-(defcustom anki-helper-model-name ""
-  "Specify the model name"
+(defcustom anki-vocabulary-model-name ""
+  "Specify the model name."
   :type 'string)
 
-(defcustom anki-helper-field-alist nil
-  "指定field的对应关系"
+(defcustom anki-vocabulary-field-alist nil
+  "指定field的对应关系."
   :type 'string)
 
-(defcustom anki-helper-audio-fileds nil
-  "Specify fields used to store audio"
+(defcustom anki-vocabulary-audio-fileds nil
+  "Specify fields used to store audio."
   :type 'list)
 
-(defcustom anki-helper-before-addnote-functions nil
+(defcustom anki-vocabulary-before-addnote-functions nil
   "List of hook functions run before add note.
 
 The functions should accept those arguments:
@@ -75,7 +76,7 @@ The functions should accept those arguments:
 + uk-phonetic(英式发音)"
   :type 'hook)
 
-(defcustom anki-helper-after-addnote-functions nil
+(defcustom anki-vocabulary-after-addnote-functions nil
   "List of hook functions run after add note.
 
 The functions should accept those arguments:
@@ -89,61 +90,64 @@ The functions should accept those arguments:
   :type 'hook)
 
 ;;;###autoload
-(defun anki-helper-set-ankiconnect ()
-  ""
+(defun anki-vocabulary-set-ankiconnect ()
+  "Set the correspondence relation of anki card."
   (interactive)
   (let ((deck-names (AnkiConnect-DeckNames))
         (model-names (AnkiConnect-ModelNames)))
-    (setq anki-helper-deck-name (completing-read "Select the Deck Name:" deck-names))
-    (setq anki-helper-model-name (completing-read "Select the Model Name:" model-names))
-    (setq anki-helper-field-alist nil)
-    (setq anki-helper-audio-fileds nil)
-    (let* ((fields (AnkiConnect-ModelFieldNames anki-helper-model-name))
+    (setq anki-vocabulary-deck-name (completing-read "Select the Deck Name:" deck-names))
+    (setq anki-vocabulary-model-name (completing-read "Select the Model Name:" model-names))
+    (setq anki-vocabulary-field-alist nil)
+    (setq anki-vocabulary-audio-fileds nil)
+    (let* ((fields (AnkiConnect-ModelFieldNames anki-vocabulary-model-name))
            (elements '("${单词}" "${释义}" "${美式音标}" "${英式音标}" "${原文例句}" "${标粗的原文例句}" "${翻译例句}" "${发声}" "SKIP")))
       (dolist (field fields)
         (let* ((prompt (format "%s" field))
                (element (completing-read prompt elements)))
           (unless (string= element "SKIP")
             (if (equal "${发声}" element)
-                (pushnew field anki-helper-audio-fileds)
+                (pushnew field anki-vocabulary-audio-fileds)
               (setq elements (remove element elements))
-              (add-to-list 'anki-helper-field-alist (cons field element)))))))))
+              (add-to-list 'anki-vocabulary-field-alist (cons field element)))))))))
 
-(defun anki-helper--get-pdf-text ()
+(defun anki-vocabulary--get-pdf-text ()
   "Get the text in pdf mode."
   (pdf-view-assert-active-region)
   (let* ((txt (pdf-view-active-region-text))
          (txt (string-join txt "\n")))
     (replace-regexp-in-string "[\r\n]+" " " txt)))
 
-(defun anki-helper--get-normal-text ()
-  "Get the text in normal mode"
+(defun anki-vocabulary--get-normal-text ()
+  "Get the text in normal mode."
   (let ((txt (if (region-active-p)
                  (buffer-substring-no-properties (region-beginning) (region-end))
                (or (sentence-at-point)
                    (thing-at-point 'line)))))
     (replace-regexp-in-string "[\r\n]+" " " txt)))
 
-(defun anki-helper--get-text ()
-  "Get the region text"
+(defun anki-vocabulary--get-text ()
+  "Get the region text."
   (if (derived-mode-p 'pdf-view-mode)
-      (anki-helper--get-pdf-text)
-    (anki-helper--get-normal-text)))
+      (anki-vocabulary--get-pdf-text)
+    (anki-vocabulary--get-normal-text)))
 
-(defun anki-helper--select-word-in-string (str &optional default-word)
-  "Select word in `STR'."
+(defun anki-vocabulary--select-word-in-string (str &optional default-word)
+  "Select word in `STR'.
+Optional argument DEFAULT-WORD specify the defauld word."
   (let ((words (split-string str "[ \f\t\n\r\v,.:?;\"<>]+")))
     (completing-read "请选择单词: " words nil nil default-word)))
 
-(defun anki-helper--get-word ()
+(defun anki-vocabulary--get-word ()
+  "Get the word at point."
   (unless (derived-mode-p 'pdf-view-mode)
     (word-at-point)))
 
 ;;;###autoload
-(defun anki-helper (&optional sentence word)
+(defun anki-vocabulary (&optional sentence word)
+  "Translate SENTENCE and WORD, and then create an anki card."
   (interactive)
-  (let* ((sentence (or sentence (anki-helper--get-text))) ; 原句
-         (word (or word  (anki-helper--select-word-in-string sentence (anki-helper--get-word))))
+  (let* ((sentence (or sentence (anki-vocabulary--get-text))) ; 原句
+         (word (or word  (anki-vocabulary--select-word-in-string sentence (anki-vocabulary--get-word))))
          (sentence_bold (replace-regexp-in-string (concat "\\b" (regexp-quote word) "\\b")
                                                   (lambda (word)
                                                     (format "<b>%s</b>" word))
@@ -171,26 +175,26 @@ The functions should accept those arguments:
                  (标粗的原文例句 . ,sentence_bold)
                  (翻译例句 . ,translation)
                  (发声 . ,(format "[sound:%s]" audio-filename))))
-         (fileds (mapcar #'car anki-helper-field-alist))
-         (elements (mapcar #'cdr anki-helper-field-alist))
+         (fileds (mapcar #'car anki-vocabulary-field-alist))
+         (elements (mapcar #'cdr anki-vocabulary-field-alist))
          (values (mapcar (lambda (e)
                            (s-format e 'aget data))
                          elements))
          (fields (cl-mapcar #'cons fileds values)))
-    (run-hook-with-args 'anki-helper-before-addnote-functions expression sentence sentence_bold translation glossary us-phonetic uk-phonetic)
-    (if anki-helper-audio-fileds
-        (let* ((audio-fileds (cond ((listp anki-helper-audio-fileds)
-                                    (apply #'vector anki-helper-audio-fileds))
-                                   ((stringp anki-helper-audio-fileds)
-                                    (vector anki-helper-audio-fileds))
+    (run-hook-with-args 'anki-vocabulary-before-addnote-functions expression sentence sentence_bold translation glossary us-phonetic uk-phonetic)
+    (if anki-vocabulary-audio-fileds
+        (let* ((audio-fileds (cond ((listp anki-vocabulary-audio-fileds)
+                                    (apply #'vector anki-vocabulary-audio-fileds))
+                                   ((stringp anki-vocabulary-audio-fileds)
+                                    (vector anki-vocabulary-audio-fileds))
                                    (t [])))
                (audio `(("url" . ,audio-url)
                         ("filename" . ,audio-filename)
                         ("fields" . ,audio-fileds))))
-          (AnkiConnect-AddNote anki-helper-deck-name anki-helper-model-name fields audio))
-      (AnkiConnect-AddNote anki-helper-deck-name anki-helper-model-name fields))
-    (run-hook-with-args 'anki-helper-after-addnote-functions expression sentence sentence_bold translation glossary us-phonetic uk-phonetic)))
+          (AnkiConnect-AddNote anki-vocabulary-deck-name anki-vocabulary-model-name fields audio))
+      (AnkiConnect-AddNote anki-vocabulary-deck-name anki-vocabulary-model-name fields))
+    (run-hook-with-args 'anki-vocabulary-after-addnote-functions expression sentence sentence_bold translation glossary us-phonetic uk-phonetic)))
 
-(provide 'anki-helper)
+(provide 'anki-vocabulary)
 
-;;; anki-helper.el ends here
+;;; anki-vocabulary.el ends here
